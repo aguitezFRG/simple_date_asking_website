1a96dd764bd3f1002b42f32aa37cbd4038dbe4b7
Eparji <aguitez.francrogerglason@gmail.com>
2026-08-01 00:45:40 +0800

    feat: preserve default invitation as demo

---
+++ app/demo/page.tsx
@@ -0,0 +1,12 @@
+import DateInvitation from "../date-invitation";
+import { getDisplayDateFromSearchParam } from "../display-date";
+
+export default async function DemoPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ date?: string | string[] }>;
+}) {
+  const { date } = await searchParams;
+
+  return <DateInvitation displayDate={getDisplayDateFromSearchParam(date)} />;
+}
